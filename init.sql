3ee18f19a99720056af3025b338c0d1cddd6ea75
Connor Ryan <Work1998@hotmail.co.uk>
2020-10-13 19:50:08 +1000

    remove db direct

--- init.sql
+++ init.sql
@@ -7,7 +7,7 @@ CREATE USER postgres WITH password postgres;
 GRANT ALL PRIVILEGES ON DATABASE production TO postgres;
 
 -- User table
-CREATE TABLE IF NOT EXISTS [production].[public].users (
+CREATE TABLE IF NOT EXISTS [public].users (
   userid SERIAL PRIMARY KEY,
   username VARCHAR NOT NULL,
   password VARCHAR NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS [production].[public].users (
 );
 
 -- Favourites table
-CREATE TABLE IF NOT EXISTS [production].[public].userFavourites (
+CREATE TABLE IF NOT EXISTS [public].userFavourites (
   id SERIAL PRIMARY KEY,
   userId INTEGER REFERENCES users(userid),
   ticker VARCHAR,
